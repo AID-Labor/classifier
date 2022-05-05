2eacdc2807a6ea201ea389d73fdbb3bbfbb166e9
encrypTimM <tim.muehle@stud.hs-bochum.de>
2022-05-05 22:00:50 +0200

    Logging-Bennenung
    Die Bennenung der Logging-Datei ist nun mit Windows kompatibel (verbotene Doppelpunkte wurden entfernt)

--- windows_auslieferung.bat
+++ windows_auslieferung.bat
@@ -5,8 +5,8 @@ set NAME=Classifier
 set DESCRIPTION=UML-Klassenmodellierung
 rem Version muss groesser gleich 1.0.0 sein!
 set VERSION=1.0.0
-set VENDOR="Tim MÅhle"
-set COPYRIGHT="Copyright © 2022 - Tim MÅhle"
+set VENDOR="Tim MÔøΩhle"
+set COPYRIGHT="Copyright ÔøΩ 2022 - Tim MÔøΩhle"
 set LICENSE_FILE=LICENSE.txt
 
 rem Einstellungen fuer jpackage:
@@ -35,7 +35,8 @@ call mvn clean install
 
 rem ---- Installer erzeugen ---------------------------------------------------------------------------
 rem ---- app-image ----
-echo %NAME%-%VERSION%-win-install.exe wird erstellt
+echo.
+echo App-Image wird erstellt
 echo.
 @echo on
 jpackage ^
