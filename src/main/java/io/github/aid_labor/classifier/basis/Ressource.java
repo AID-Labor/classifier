7e07e2e8c9d83566e444e9903fd2b371de2d54d6
encrypTimM <tim.muehle@stud.hs-bochum.de>
2022-05-05 18:12:13 +0200

    Ressourcen jmod
    Die Ressourcen werden auch aus einer von jlink erstellten jmod entpackt. fixed #2, closes #2

--- src/main/java/io/github/aid_labor/classifier/basis/Ressource.java
+++ src/main/java/io/github/aid_labor/classifier/basis/Ressource.java
@@ -6,13 +6,12 @@
 
 package io.github.aid_labor.classifier.basis;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 
 
-public record Ressource(String ordnerPfad, String name) {
+public abstract sealed class Ressource permits KlassenpfadRessource, LokaleRessource {
 	
 // ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
 // #                                                                              		      #
@@ -20,27 +19,30 @@ public record Ressource(String ordnerPfad, String name) {
 // #																						  #
 // ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
 	
+//	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+//  *	Attribute																		*
+//	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	
+	protected final String pfad;
+	
 //	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 //  *	Konstruktoren																		*
 //	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	
-	// impliziter Konstruktor fuer die Record-Komponenten
+	Ressource(String pfad) {
+		this.pfad = pfad;
+	}
+	
+	Ressource(String ordner, String name) {
+		this(ordner + OS.getDefault().seperator + name);
+	}
 	
 //	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 //  *	Methoden																		*
 //	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	
-	public Path alsPath() {
-		if (this.ordnerPfad == null) {
-			return Path.of(this.name);
-		} else if (this.name == null) {
-			return Path.of(this.ordnerPfad);
-		} else {
-			return Path.of(this.ordnerPfad, this.name);
-		}
-	}
+	public abstract Path alsPath();
 	
-	public InputStream oeffneStream() throws FileNotFoundException {
-		return new FileInputStream(alsPath().toFile());
-	}
+	public abstract InputStream oeffneStream() throws IOException; 
+	
 }
