cbf13c6eb67d8776f42e63427282a6cd259c9bb8
encrypTimM <tim.muehle@stud.hs-bochum.de>
2022-06-15 22:02:58 +0200

    Release 1.0.1 [PREVIEW] - [SNAPSHOT_002]
    — Features ————————————————————
    
        - Kommentar-Element
        - z-Anordnung im Diagramm
        - Kopieren, Einfügen, Löschen
        - Einstellungen: Void + Parameternamen umschalten
        - Menüaktionen für
            - Rückgängig
            - Wiederholen
            - Neue Klasse
            - Neue abstrakte Klasse
            - Neues Interface
            - Neue Enumeration
            - Kleiner Zoomen
            - Größer Zoomen
            - Originalgröße
        - Neuer Button für abstrakte Klassen hinzugefügt, Button für Enumeration entfernt
    
    — Fixes —————————————————————
    
        - Rückgängig-Funktion - Verbesserung Speicherstatus
        - Behebt einen Fehler, der dazu geführt hat, dass eine Editierung unter Umständen nicht rückgängig gemacht werden konnte
        - Dialog - Anpassung und Verbesserung der Größe
        - Zeichenfläche - automatische Größenanpassung
        - Behebt den Fehler, dass abstrakt ggf. nicht bei Methoden in abstrakten Typen ausgewählt werden konnte

--- classifier.sh
+++ classifier.sh
@@ -1,3 +1,3 @@
-VERSION=1.0.1
+VERSION=1.0.2
 DIR=auslieferung/${VERSION}/macOS/input
 java --module-path ${DIR}/lib --add-modules ALL-MODULE-PATH -jar ${DIR}/classifier-${VERSION}.jar classifier/is.github.aid_labor.classifier.basis.Ressourcen $@
