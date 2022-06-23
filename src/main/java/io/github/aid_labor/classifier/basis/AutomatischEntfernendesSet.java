04a511ba221cd2f550cf563356402fe339079aa6
encrypTimM <tim.muehle@stud.hs-bochum.de>
2022-06-23 22:45:30 +0200

    javadoc

--- src/main/java/io/github/aid_labor/classifier/basis/AutomatischEntfernendesSet.java
+++ src/main/java/io/github/aid_labor/classifier/basis/AutomatischEntfernendesSet.java
@@ -11,15 +11,15 @@ import java.util.logging.Logger;
 
 
 /**
- * Set, das eine maximale Groesse hat. Wenn beim Hinzufuegen die maximale Groesse
- * ueberschritten wird, wird das kleinste Element automatisch entfernt.
+ * Sortiertes Set, das eine maximale Groesse hat. Wenn beim Hinzufügen die maximale Groesse
+ * überschritten wird, wird das kleinste Element (basierend auf der natürlichen
+ * Ordnung der Elemente, siehe {@link TreeSet}, {@link Set}, {@link Comparable}) automatisch entfernt.
  * 
  * @author Tim Muehle
  *
  */
 public class AutomatischEntfernendesSet<E> extends TreeSet<E> {
-	private static final Logger log = Logger
-			.getLogger(AutomatischEntfernendesSet.class.getName());
+	private static final Logger log = Logger.getLogger(AutomatischEntfernendesSet.class.getName());
 	
 	private static final long serialVersionUID = -3027636633260070772L;
 	
@@ -39,6 +39,13 @@ public class AutomatischEntfernendesSet<E> extends TreeSet<E> {
 //  *	Konstruktoren																		*
 //	* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	
+	/**
+	 * Erzeugt ein neues automatisch entfernendes Set, das Elemente basierend auf der natürlichen Ordnung
+	 * durch Implementierung von {@link Comparable} sortiert. Das Set hat Platz für die übergebene Anzahl
+	 * an Elementen.
+	 * 
+	 * @param maximaleAnzahl maximale Größe des Sets
+	 */
 	public AutomatischEntfernendesSet(int maximaleAnzahl) {
 		super();
 		if (maximaleAnzahl < 1) {
@@ -53,6 +60,10 @@ public class AutomatischEntfernendesSet<E> extends TreeSet<E> {
 	
 // public	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##
 	
+	/**
+	 * maximale Anzahl der Enthaltenen Elemente
+	 * @return maximale Anzahl der Enthaltenen Elemente
+	 */
 	public int getMaximaleAnzahl() {
 		return maximaleAnzahl;
 	}
@@ -69,6 +80,13 @@ public class AutomatischEntfernendesSet<E> extends TreeSet<E> {
 	
 // public	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##	##
 	
+	/**
+	 * Fügt ein Element hinzu, falls dieses noch nicht im Set enthalten ist.
+	 * Wird dabei die maxiamle Größe überschritten, wird das kleinste Element
+	 * automatisch entfernt.
+	 * 
+	 * @param e einzufügendes Element
+	 */
 	@Override
 	public boolean add(E e) {
 		boolean hinzugefuegt = super.add(e);
